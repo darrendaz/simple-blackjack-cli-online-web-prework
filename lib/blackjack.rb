6b1ad4f17a275b6093ff20a71bbcb8308c1087fb
Darren L <darren.lou@gmail.com>
2019-02-18 22:28:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -69,10 +69,10 @@ def runner
   # code runner here
   welcome
   initial_round
-  until card_total > 21
-    hit?(card_total)
-    display_card_total(card_total)
-  end
+  # until card_total > 21
+  #   hit?(card_total)
+  #   display_card_total(card_total)
+  # end
   
   end_game(card_total)
 end
